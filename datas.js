1cdd1dcc8af3b2518e482c7588c953f18f309364
nikodego <nikkobryllealturas716@gmail.com>
2023-11-17 18:47:32 +1100

    1st commit

---
+++ datas.js
@@ -0,0 +1,92 @@
+export const data = [
+    {
+        id:'1',
+        name: 'Monkey D. Luffy',
+        alias:'Straw Hat',
+        address:'East Blue',
+        bounty:'3,000,000,000',
+        avatar:
+            'https://i.pinimg.com/originals/f0/af/cb/f0afcbce7ed4a7df7b822964501bf995.jpg',
+    },
+    {
+        id:'2',
+        name: 'Roronoa Zoro',
+        alias:'Pirate Hunter',
+        address:'East Blue',
+        bounty:'1,111,000,000',
+        avatar:
+            'https://images4.alphacoders.com/122/1228259.png',
+    },
+    {
+        id:'3',
+        name: 'Nami',
+        alias:'Cat Burglar',
+        address:'East Blue',
+        bounty:'366,000,000',
+        avatar:
+            'https://staticg.sportskeeda.com/editor/2023/03/474e0-16794903651247-1920.jpg?w=840',
+    },
+    {
+        id:'4',
+        name: 'Ussop',
+        alias:'Sogeking',
+        address:'East Blue',
+        bounty:'500,000,000',
+        avatar:
+            'https://media.comicbook.com/2019/09/usopp-one-piece-1188276.jpeg?auto=webp',
+    },
+    {
+        id:'5',
+        name: 'Sanji',
+        alias:'Black Leg',
+        address:'North Blue',
+        bounty:'1,032,000,000',
+        avatar:
+            'https://staticg.sportskeeda.com/editor/2023/05/425f5-16829548801476-1920.jpg?w=840',
+    },
+    {
+        id:'6',
+        name: 'Tony Tony Chopper',
+        alias:'Cotton Candy Lover',
+        address:'Grand Line',
+        bounty:'1,000',
+        avatar:
+            'https://i.pinimg.com/564x/c0/c7/1b/c0c71bcadc86be5ea1c9193e71e3b05a.jpg',
+    },
+    {
+        id:'7',
+        name: 'Nico Robin',
+        alias:'Demon Child',
+        address:'West Blue',
+        bounty:'930,000,000',
+        avatar:
+            'https://cdn.amodays.com/20ad1a13c551da3eba8e54110be91c29.jpg',
+    },
+    {
+        id:'8',
+        name: 'Franky',
+        alias:'Cyborg',
+        address:'Grand Line',
+        bounty:'394,000,000',
+        avatar:
+            'https://staticg.sportskeeda.com/editor/2023/05/50b87-16836967920057-1920.jpg?w=840',
+    },
+    {
+        id:'9',
+        name: 'Brook',
+        alias:'Soul King',
+        address:'West Blue',
+        bounty:'383,000,000',
+        avatar:
+            'https://lh4.googleusercontent.com/nL6FEg_LjLIp7uLl-aeN26JOsMkPCL9NiggwQRpBprGV-I5Uu6otVFLZNf45GOX7lUdP2I-bQJvZL8tJ6mu3kHbaIV-ucscUiEUo_Lir22Jwzf-uzIasdfQjWVmlYVkU2C7pKtosNA-v2oQwqGjl0co',
+    },
+    {
+        id:'10',
+        name: 'Jinbei',
+        alias:'anon',
+        address:'Grand Line',
+        bounty:'1,100,000,000',
+        avatar:
+            'https://i.pinimg.com/originals/d0/d5/91/d0d59102cf996bdd113c54019099fea1.jpg',
+    },
+];
